76a690c4ffa1067b92e4a0f987461362b6c132a1
Diego <omenegazzi@gmail.com>
2020-05-23 14:27:14 -0300

    Ajustado conflito

--- app/Model/Disciplina.php
+++ app/Model/Disciplina.php
@@ -3,7 +3,6 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-<<<<<<< HEAD
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Disciplina extends Model
@@ -21,21 +20,5 @@ class Disciplina extends Model
     public function professor()
     {
         return $this->belongsTo('App\Model\Professor','professor_id');
-=======
-
-class Disciplina extends Model
-{
-    protected $table = 'disciplinas';
-
-    protected $fillable = [
-        'nome','professor_id'
-    ];
-
-    public $timestamps = false;
-
-    public function cidade()
-    {
-        return $this->belongsTo('App\Model\Disciplina','professor_id');
->>>>>>> features/Diego
     }
 }
