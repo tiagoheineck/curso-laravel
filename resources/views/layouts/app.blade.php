de1944e7b088048aad01d3429d5abcefd10b6547
Tiago Heineck <tiago.heineck@ifc.edu.br>
2020-05-25 10:20:11 -0300

    esqueci na babaquice de salvar um arquivo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,7 +56,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    @can('isAdmin2',$disciplina)
+                                    @can('isAdmin')
                                     <a class="dropdown-item" href="{{ route('disciplinas.index') }}">
                                         Disciplinas
                                     </a>
